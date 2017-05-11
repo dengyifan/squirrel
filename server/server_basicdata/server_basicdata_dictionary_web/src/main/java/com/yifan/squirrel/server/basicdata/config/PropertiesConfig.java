8eda15ef4c61dde12dc1b99e8813fbbbb7e5e4ce
dengyifan <waynesilver@163.com>
2017-05-11 22:04:55 +0800

    添加 server_config

--- server/server_basicdata/server_basicdata_dictionary_web/src/main/java/com/yifan/squirrel/server/basicdata/config/PropertiesConfig.java
+++ server/server_basicdata/server_basicdata_dictionary_web/src/main/java/com/yifan/squirrel/server/basicdata/config/PropertiesConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.PropertySource;
  */
 @Configuration
 @PropertySource({
-        "classpath:com/yifan/squirrel/server/basic/config/jdbc.properties"
+        "classpath:com/yifan/squirrel/server/config/dev/jdbc.properties"
 })
 public class PropertiesConfig {
 }
