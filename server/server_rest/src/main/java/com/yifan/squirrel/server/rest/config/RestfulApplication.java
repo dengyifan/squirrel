9cea7f4439f9ede0192ce90369527a4cbcbbf23f
dengyifan <waynesilver@163.com>
2017-05-26 22:25:05 +0800

    添加 server_rest JAX-RS 官网地址/docs.oracle.com/cd/E19226-01/820-7627/index.html

---
+++ server/server_rest/src/main/java/com/yifan/squirrel/server/rest/config/RestfulApplication.java
@@ -0,0 +1,22 @@
+package com.yifan.squirrel.server.rest.config;
+
+import com.yifan.squirrel.server.rest.controller.HelloController;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by yifan on 17-5-26.
+ */
+@ApplicationPath("/server/resources")
+public class RestfulApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(HelloController.class);
+        return classes;
+    }
+}
---
+++ server/server_rest/src/main/java/com/yifan/squirrel/server/rest/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.yifan.squirrel.server.rest.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * Created by yifan on 17-5-26.
+ */
+@Path("hello")
+public class HelloController {
+
+    @Context
+    private UriInfo context;
+
+    public HelloController(){}
+
+    @GET
+    @Produces("text/html")
+    public String getHtml(){
+        return "<html lang=\"en\"><body><h1>Hello,World!</h1></body></html>";
+    }
+}
