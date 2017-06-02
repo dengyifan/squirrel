069d91ea5a1c516e6d8b90eee1677dfece503997
yifan <waynesilver@163.com>
2017-06-02 10:33:41 +0800

    修正 websocket 示例

--- browser/browser_chat/src/main/java/com/yifan/squirrel/browser/chat/server/config/ChatEndpoint.java
+++ browser/browser_chat/src/main/java/com/yifan/squirrel/browser/chat/server/config/ChatEndpoint.java
@@ -1,47 +1,98 @@
 package com.yifan.squirrel.browser.chat.server.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by yifan on 2017/6/1.
+ *
+ * 源码见：
+ * http://svn.apache.org/viewvc/tomcat/tc7.0.x/trunk/webapps/examples/WEB-INF/classes/websocket/chat/ChatAnnotation.java?view=markup
  */
-@ServerEndpoint("/chatroom")
+@ServerEndpoint("/chatroom/{roomName}")
 public class ChatEndpoint {
 
-    Logger logger = LoggerFactory.getLogger(ChatEndpoint.class);
+    private static final Log log = LogFactory.getLog(ChatEndpoint.class);
 
-    @OnOpen
-    public void open(Session session){
-        logger.info("ChatEndpoint open roomName:" );
+    private static final String GUEST_PREFIX = "Guest";
+
+    private static final AtomicInteger connectionIds = new AtomicInteger(0);
+
+    private static final Set<ChatEndpoint> connections = new CopyOnWriteArraySet<ChatEndpoint>();
+
+    private final String nickName;
+    private Session session;
+
+
+    public ChatEndpoint(){
+        nickName = GUEST_PREFIX + connectionIds.getAndIncrement();
     }
 
-    @OnMessage
-    public void message(Session session,String msg) {
-        try{
+    @OnOpen
+    public void open(Session session, @PathParam("roomName") String roomName){
+        log.info("ChatEndpoint open roomName:" + roomName);
 
-            logger.info("ChatEndpoint message roomName: "  + " msg:" + msg);
-            for(Session curSession : session.getOpenSessions()) {
-                if(curSession.isOpen()){
-                    curSession.getBasicRemote().sendText(" : "+msg);
-                }
-            }
-        }catch(IOException ie){
-            logger.error(ie.getStackTrace().toString());
-        }
+        this.session = session;
+        connections.add(this);
 
+        String message = String.format("* %s %s", nickName, "has joined.");
+        broadcast(message);
     }
 
 
     @OnClose
-    public void close(Session session) {
-        logger.info("ChatEndpoint close roomName:");
+    public void close(Session session, @PathParam("roomName") String roomName) {
+        log.info("ChatEndpoint close roomName:" + roomName);
+        connections.remove(this);
+        String message = String.format("* %s %s",nickName, "has disconnected.");
+        broadcast(message);
     }
+
+
+
+    @OnMessage
+    public void message(Session session,String msg,@PathParam("roomName") String roomName) {
+        log.info("ChatEndpoint message roomName: " + roomName + " msg:" + msg);
+        String filteredMessage = String.format("%s: %s",nickName, msg);
+        broadcast(filteredMessage);
+
+    }
+
+    @OnError
+    public void error(Session session,Throwable t){
+        log.error("ChatEndpoint Error: " + t.toString(), t);
+
+    }
+
+
+    private static void broadcast(String msg) {
+        for (ChatEndpoint client : connections) {
+            try {
+                synchronized (client) {
+                    client.session.getBasicRemote().sendText(msg);
+                }
+            } catch (IOException e) {
+                log.debug("ChatEndpoint Error: Failed to send message to client", e);
+                connections.remove(client);
+                try {
+                    client.session.close();
+                } catch (IOException e1) {
+                    // Ignore
+                }
+                String message = String.format("* %s %s",
+                        client.nickName, "has been disconnected.");
+                broadcast(message);
+            }
+        }
+    }
+
+
 }
--- browser/browser_chat/src/main/webapp/index.jsp
+++ browser/browser_chat/src/main/webapp/index.jsp
@@ -8,13 +8,26 @@
     <script type="text/javascript">
 
 
+        var wsocket = new WebSocket("ws://localhost:8500/browser_chat/chatroom/music");
+
+        // 打开Socket
+        wsocket.onopen = function(event) {
+
+            // 发送一个初始化消息
+            wsocket.send('I am the client and I\'m listening!');
+
+            // 监听消息
+            wsocket.onmessage = function(event) {
+                console.log('Client received a message',event);
+            };
+
+            // 监听Socket的关闭
+            wsocket.onclose = function(event) {
+                console.log('Client notified socket has closed',event);
+            };
+        };
 
         function sendMsg() {
-
-            var wsocket = new WebSocket("ws://localhost:8500/browser_chat/chatroom");
-            wsocket.onmessage = function(evt){
-                alert(evt.data);
-            };
 
             var msgObj = document.getElementById('msg');
             wsocket.send(msgObj.value)
